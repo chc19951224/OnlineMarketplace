9ab92b04d4aacdabd939887a0c7918e315cdc2b8
chc19951224 <chc19951224@outlook.com>
2025-06-04 17:23:27 +0800

    版本 3 1. 修改首頁母版：修改前台母版脚本文件的綁定、圖片區域html容器元素屬性，解決圖片的異常顯示。 2. 新增控制器和視圖：新增後台首頁控制器、視圖。 3. 新增自定義主題：新增後台自定義主題相關的 css 、 js 、 images 等文件。 4. 新增捆綁文件：新增定義主題的脚本、樣式文件和引用路徑，並新增對應的捆綁文件。 5. 新增首頁母版：新增後台部分視圖，在首頁母版引用模塊化的部分視圖内容，明確各個區域的功能。 6. 新增前後台超鏈接：修改前台首頁按鈕，引用控制器方法跳轉到後台，前台的做法也一樣，可以跳轉到前台，至此完成了前後台的連通。

--- OnlineMarketplace.Web/App_Start/BundleConfig.cs
+++ OnlineMarketplace.Web/App_Start/BundleConfig.cs
@@ -7,53 +7,59 @@ namespace OnlineMarketplace.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            #region 【 JavaScript 文件 】
-
-            #region 【 JS 文件類型支援的捆綁 】
-            bundles.Add(new ScriptBundle("~/bundles/support/jquery").Include(
-                        "~/Content/ScriptsSupport/jquery-{version}.js"));
-
+            #region 【 JS 文件 】
+            // 官方 JS 文件
             bundles.Add(new ScriptBundle("~/bundles/support/jqueryval").Include(
-                        "~/Content/ScriptsSupport/jquery.validate*"));
-
+                        "~/Content/ScriptsSupport/jquery.validate*")); // 未使用
             bundles.Add(new ScriptBundle("~/bundles/support/modernizr").Include(
                         "~/Content/ScriptsSupport/modernizr-*"));
 
-            // 自定義 JS 文件類型支援的捆綁
-            bundles.Add(new Bundle("~/bundles/support/bootstrap").Include(
-                      "~/Content/FrontendTheme/js/bootstrap.bundle.min.js"));
-            #endregion
-
-            #region 【 Frontend 捆綁自定義的 Bootstrap 所需脚本 】
-            bundles.Add(new ScriptBundle("~/bundles/FrontendTheme/js").Include(
-                        //"~/Content/FrontendTheme/js/bootstrap.bundle.min.js",
-                        "~/Content/FrontendTheme/js/jquery.min.js",
-                        "~/Content/FrontendTheme/js/modernizr.js",
+            #region 【 Frontend 第三方主題 JS 捆綁和支援 】
+            bundles.Add(new ScriptBundle("~/bundles/frontend/js").Include(
                         "~/Content/FrontendTheme/js/plugins.js",
-                        "~/Content/FrontendTheme/js/script.min.js",
-                        "~/Content/FrontendTheme/js/SmoothScroll.js"));
+                        "~/Content/FrontendTheme/js/SmoothScroll.js",
+                        "~/Content/FrontendTheme/js/script.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/frontend/support/jquery").Include(
+                      "~/Content/FrontendTheme/js/jquery.min.js"));
+            bundles.Add(new Bundle("~/bundles/frontend/support/bootstrap").Include(
+                      "~/Content/FrontendTheme/js/bootstrap.bundle.min.js"));
+            bundles.Add(new Bundle("~/bundles/frontend/support/swiper").Include(
+                      "~/Content/FrontendTheme/js/swiper-bundle.min.js"));
             #endregion
 
+            #region 【 Backend 第三方主題 JS 捆綁和支援 】
+            bundles.Add(new ScriptBundle("~/bundles/backend/js").Include(
+                        "~/Content/BackendTheme/js/jquery.easing.min.js",
+                        "~/Content/BackendTheme/js/sb-admin-2.min.js",
+                        "~/Content/BackendTheme/js/Chart.min.js",
+                        "~/Content/BackendTheme/js/chart-area-demo.js",
+                        "~/Content/BackendTheme/js/chart-pie-demo.js"));
+
+            bundles.Add(new Bundle("~/bundles/backend/support/jquery").Include(
+                      "~/Content/BackendTheme/js/jquery.min.js"));
+            bundles.Add(new Bundle("~/bundles/backend/support/bootstrap").Include(
+                      "~/Content/BackendTheme/js/bootstrap.bundle.min.js"));
             #endregion
+            #endregion
+
 
             #region 【 CSS 文件 】
-
-            #region 【 CSS 文件類型支援的捆綁 】
-            bundles.Add(new StyleBundle("~/Content/support/css").Include(
-                      "~/Content/DefaultTheme/css/bootstrap.css",
-                      "~/Content/DefaultTheme/css/site.css"));
-            #endregion
-
-            #region 【 Frontend 捆綁自定義的 CSS 所需脚本 】
-            bundles.Add(new StyleBundle("~/bundles/FrontendTheme/css").Include(
+            #region 【 Frontend 第三方主題 CSS 捆綁 】
+            bundles.Add(new StyleBundle("~/bundles/frontend/css").Include(
                         "~/Content/FrontendTheme/css/bootstrap.min.css",
-                        "~/Content/FrontendTheme/css/fonts.googleapis.com.css",
-                        "~/Content/FrontendTheme/css/normalize.css",
-                        "~/Content/FrontendTheme/css/style.css",
+                        "~/Content/FrontendTheme/css/vendor.css",
                         "~/Content/FrontendTheme/css/swiper-bundle.min.css",
-                        "~/Content/FrontendTheme/css/vendor.css"));
+                        "~/Content/FrontendTheme/css/style.css",
+                        "~/Content/FrontendTheme/css/fonts.googleapis.com.css"));
             #endregion
 
+            #region 【 Backend 第三方主題 CSS 捆綁 】
+            bundles.Add(new StyleBundle("~/bundles/backend/css").Include(
+                        "~/Content/BackendTheme/css/all.min.css",
+                        "~/Content/BackendTheme/css/fonts.googleapis.com.css",
+                        "~/Content/BackendTheme/css/sb-admin-2.min.css"));
+            #endregion
             #endregion
         }
     }
---
+++ OnlineMarketplace.Web/Controllers/BackendController.cs
@@ -0,0 +1,18 @@
+﻿using OnlineMarketplace.Services;
+using OnlineMarketplace.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineMarketplace.Web.Controllers
+{
+    public class BackendController : Controller
+    {
+        public ActionResult BackendIndex()
+        {
+            return View();
+        }
+    }
+}
